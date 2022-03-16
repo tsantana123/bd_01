148e0b4d3f7dcd1b5007eabeae5b129dceeff714
tsantana123 <naraaraujosantana@gmail.com>
2022-03-15 21:50:18 -0400

    it

---
+++ pessoa_fisica.sql
@@ -0,0 +1,43 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 16-Mar-2022 às 02:49
+-- Versão do servidor: 10.4.22-MariaDB
+-- versão do PHP: 8.0.15
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `itau`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pessoa_fisica`
+--
+
+CREATE TABLE `pessoa_fisica` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(50) NOT NULL,
+  `cpf` varchar(11) NOT NULL,
+  `telefone` varchar(12) NOT NULL,
+  `agencia` varchar(10) NOT NULL,
+  `conta` varchar(10) NOT NULL,
+  `pix` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
